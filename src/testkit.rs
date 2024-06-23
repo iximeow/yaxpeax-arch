288f0b64a03fe99a6765298a181dfb4ae41049b5
iximeow <me@iximeow.net>
2024-06-23 16:16:45 -0700

    doc comments for the new testkit module

--- src/testkit.rs
+++ src/testkit.rs
@@ -1,2 +1,8 @@
+//! utilities to validate that implementations of traits in `yaxpeax-arch` uphold requirements
+//! described in this crate.
+//!
+//! currently, this only includes tools to validate correct use of
+//! [`yaxpeax_arch::display::DisplaySink`], but may grow in the future.
+
 #[cfg(feature="alloc")]
 pub mod display;
